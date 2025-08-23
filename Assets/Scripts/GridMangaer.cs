c827f9d6797251d51ed031f7493c1d9a79586d34
Keybats <smule2006@outlook.com>
2025-08-23 12:51:20 +0300

    building system

--- Assets/Scripts/GridMangaer.cs
+++ Assets/Scripts/GridMangaer.cs
@@ -1,14 +1,24 @@
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using UnityEngine.InputSystem;
 public class GridMangaer: MonoBehaviour
 {
+    [SerializeField] Camera mainCamera;
+
     [SerializeField] int mapHeight;
     [SerializeField] int mapWidth;
 
+    [SerializeField] int waterLevel;
+
+    [SerializeField] TMP_Text text;
+
+    [SerializeField] List<string> buildingNames;
+
     [SerializeField] TileBase ground;
-    [SerializeField] TileBase BigHouse;
-    [SerializeField] TileBase LittleHouse;
+    [SerializeField] TileBase bigHouse;
+    [SerializeField] TileBase littleHouse;
     [SerializeField] TileBase warehouse;
     [SerializeField] TileBase sawmill;
     [SerializeField] TileBase farm;
@@ -17,13 +27,21 @@ public class GridMangaer: MonoBehaviour
     [SerializeField] TileBase merchantDock;
     [SerializeField] TileBase infirmary;
 
+    
+
+    [SerializeField] TileBase placeableSquare;
+
+    List<TileBase> buildings = new List<TileBase>();
+    List<Cell.TileType> noDemolish = new List<Cell.TileType> { Cell.TileType.Air, Cell.TileType.Ground, Cell.TileType.Church };
+
     [SerializeField] Tilemap tilemap;
 
-    TileBase selectedBuilding;
+    int selectedBuilding = 0;
 
     Cell[,] cellGrid;
     void Start()
     {
+        buildings = new List<TileBase> { bigHouse, littleHouse, warehouse, sawmill, farm, fishDocks, merchantDock, infirmary, null };
         cellGrid = new Cell[mapWidth, mapHeight];
         ReadMap();
     }
@@ -36,15 +54,15 @@ public class GridMangaer: MonoBehaviour
             {
                 TileBase currentTile = tilemap.GetTile(new Vector3Int(i, j));
                 Cell newCell = new Cell();
-                if (currentTile == ground)
+                if (ground == currentTile)
                 {
                     newCell.type = Cell.TileType.Ground;
                 }
-                else if (currentTile == BigHouse)
+                else if (currentTile == bigHouse)
                 {
                     newCell.type = Cell.TileType.BigHouse;
                 }
-                else if (currentTile == LittleHouse)
+                else if (currentTile == littleHouse)
                 {
                     newCell.type = Cell.TileType.LittleHouse;
                     newCell.canBuildAbove = false;
@@ -66,9 +84,8 @@ public class GridMangaer: MonoBehaviour
                 {
                     newCell.type = Cell.TileType.FishDocks;
                     newCell.canBuildAbove = false;
-                    newCell.canBuildInland = false;
                 }
-                else if (currentTile == fishDocks)
+                else if (currentTile == church)
                 {
                     newCell.type = Cell.TileType.Church;
                     newCell.canBuildAbove = false;
@@ -76,6 +93,16 @@ public class GridMangaer: MonoBehaviour
                 else if (currentTile == infirmary)
                 {
                     newCell.type = Cell.TileType.Infirmary;
+                    newCell.canBuildAbove = false;
+                }
+                else if (currentTile == merchantDock)
+                {
+                    newCell.type = Cell.TileType.MerchantDock;
+                    newCell.canBuildAbove = false;
+                }
+                else if (currentTile == placeableSquare)
+                {
+                    newCell.type = Cell.TileType.PlaceableSquare;
                     newCell.canBuildAbove = false;
                 }
                 else
@@ -91,17 +118,101 @@ public class GridMangaer: MonoBehaviour
         }
     }
 
+    public void CycleBuilding(int direction)
+    {
+        if (selectedBuilding == 0 && direction == -1)
+        {
+            selectedBuilding = buildings.Count - 1;
+        }
+        else if (selectedBuilding == buildings.Count - 1 && direction == 1)
+        {
+            selectedBuilding = 0;
+        }
+        else
+        {
+            selectedBuilding += direction;
+        }
+        text.text = buildingNames[selectedBuilding];
+        DetectPlaceableSquares();
+    }
+    
+    void DetectPlaceableSquares()
+    {
+        bool canBePlacedInland = buildingNames[selectedBuilding] != "Fish docks";
+        Debug.Log(canBePlacedInland);
+        if (buildings[selectedBuilding] == null)
+        {
+            for (int i = 0; i < cellGrid.GetLength(0); i++)
+            {
+                for (int j = 1; j < cellGrid.GetLength(1); j++)
+                {
+                    if (cellGrid[i, j].type == Cell.TileType.PlaceableSquare)
+                    {
+                        ReplaceTile(null, new Vector3Int(i, j));
+                    }
+
+                }
+            }
+        }
+        else if (canBePlacedInland)
+        {
+            for (int i = 0; i < cellGrid.GetLength(0); i++)
+            {
+                for (int j = 1; j < cellGrid.GetLength(1); j++)
+                {
+                    if (cellGrid[i, j].type == Cell.TileType.Air && cellGrid[i, j - 1].canBuildAbove)
+                    {
+                        ReplaceTile(placeableSquare, new Vector3Int(i, j));
+                    }
+
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < cellGrid.GetLength(0); i++)
+            {
+                for (int j = 1; j < cellGrid.GetLength(1); j++)
+                {
+                    if (cellGrid[i, j].type == Cell.TileType.Air && cellGrid[i, j - 1].canBuildAbove && j <= waterLevel)
+                    {
+                        ReplaceTile(placeableSquare, new Vector3Int(i, j));
+                    }
+                    else if (cellGrid[i, j].type == Cell.TileType.PlaceableSquare && j > waterLevel )
+                    {
+                        ReplaceTile(null, new Vector3Int(i, j));
+                    }
+                    
+                }
+            }
+        }
+        ReadMap();
+    }
+
     private void Update()
     {
-        if (Keyboard.current.digit1Key.wasPressedThisFrame)
+        if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-            selectedBuilding = BigHouse;
+            Vector3Int mousePos = tilemap.WorldToCell(mainCamera.ScreenToWorldPoint(Mouse.current.position.value));
+            if (0 <= mousePos.x && mousePos.x < cellGrid.GetLength(0) && 0 <= mousePos.y && mousePos.y < cellGrid.GetLength(1))
+            {
+                Debug.Log(mousePos);
+                if (cellGrid[mousePos.x, mousePos.y].type == Cell.TileType.PlaceableSquare)
+                {
+                    ReplaceTile(buildings[selectedBuilding], mousePos);
+                    ReadMap();
+                    DetectPlaceableSquares();
+                }
+                else if (!noDemolish.Contains(cellGrid[mousePos.x, mousePos.y].type) && buildings[selectedBuilding] == null && (cellGrid[mousePos.x, Mathf.Clamp(mousePos.y + 1, 0, cellGrid.GetLength(1) -   1)].type == Cell.TileType.Air || mousePos.y == cellGrid.GetLength(1) - 1) ) {
+                    ReplaceTile(buildings[selectedBuilding], mousePos);
+                    ReadMap();
+                }
+            }
         }
     }
 
     public void ReplaceTile(TileBase newTile, Vector3Int coordinates)
     {
         tilemap.SetTile(coordinates, newTile);
-        ReadMap();
     }
 }
--- Assets/Scripts/PlaneC#/Cell.cs
+++ Assets/Scripts/PlaneC#/Cell.cs
@@ -3,21 +3,21 @@ using UnityEngine;
 public class Cell
 {
     public enum TileType {
-        Air, // can't build above
-        Ground,
+        Air, // can't build and can't build above
+        Ground, // can't build
         LittleHouse, // can't build above
         BigHouse,
         Sawmill,
         Farm, // can't build above 
         Warehouse,
         MerchantDock, // can't build above
-        Infirmary, //can't build
-        FishDocks, // can't build above, can't build inland
-        Church // can't build
+        Infirmary, //can't build above
+        FishDocks, // can't build above or away from water
+        Church, // can't build and can't build above
+        PlaceableSquare // can't build and can't build
     }
 
     public int[] position = { 0, 0 };
     public TileType type = TileType.Air;
     public bool canBuildAbove = true;
-    public bool canBuildInland = true;
 }
