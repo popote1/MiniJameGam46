034af602db62d0bbeca1b30f91083e3d33cab2b2
popote1 <yvan.bladet@gmail.com>
2025-08-24 17:35:51 +0200

    addid levels

---
+++ Assets/Scripts/GameObjectLevel2Manager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class GameObjectLevel2Manager : GameObjectiveManager
+{
+
+    [SerializeField] private int FoodToGet = 600;
+    [SerializeField, TextArea] private string _winMessage;
+
+    protected override void StaticEventOnOnDoGameTick(object sender, EventArgs e) {
+        if (StaticData.CurrentFood >= FoodToGet) {
+            PlayWin(_winMessage);
+        }
+        base.StaticEventOnOnDoGameTick(sender, e);
+    }
+}
---
+++ Assets/Scripts/GameObjectLevel3Manager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class GameObjectLevel3Manager : GameObjectiveManager {
+
+    [SerializeField] private int GoldToGet = 600;
+    [SerializeField, TextArea] private string _winMessage;
+
+    protected override void StaticEventOnOnDoGameTick(object sender, EventArgs e) {
+        if (StaticData.Gold >= GoldToGet) {
+            PlayWin(_winMessage);
+        }
+        base.StaticEventOnOnDoGameTick(sender, e);
+    }
+}
